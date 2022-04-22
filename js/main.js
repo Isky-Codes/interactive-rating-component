a103244bb63cf963eb8505a1e757f96195b8f55a
Isky-Codes <95683156+Isky-Codes@users.noreply.github.com>
2022-04-22 15:57:16 +0800

    Coded ability to choose rating and insert it into <span> in 'Thank You' card, added submit button functionality

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-//When a button is clicked with the corresponding 'Rating' number, this value is saved such that it can be re-used later in the 'Thank You' section to indicate which number was chosen.
-
-//When the submit button is clicked, either:
-//  -If number was not chosen, alert the user to pick a number.
-//  -If number was chosen, save the value to be reused and hide the 'Rating' card and show the 'Thank You' card.
-
-//On the 'Thank You' card add the chosen number to the <span> to indicate which number was chosen to the user.
-
-//Creating the submit button functionality to remove the 'Rating' card and show the 'Thank You' card.
-
 const submitBtn = document.querySelector('.submitButton');
 const ratingCard = document.querySelector('.ratingContainer');
 const thankYouCard = document.querySelector('.thankYouContainer');
 const buttons = document.querySelectorAll('.ratingButtons');
+const chosenRating = document.querySelector('#chosenNumber');
+
+
+/*Function for submit button where upon click it will switch to the 'Thank You' card. If a rating was not chosen, an alert will sound*/
+submitBtn.addEventListener('click', onSubmitClick);
+
+function onSubmitClick() {
+    if(!chosenRating.textContent) {
+        alert('Please select a rating to provide us with your feedback!')
+    }else {
+        ratingCard.style.display = 'none';
+        thankYouCard.style.display = 'flex';
+    }
+}
+
+
+/*Function for selecting a rating, saving the value and inserting the value into the <span> on the 'Thank You' card to indicate which rating was chosen*/
 buttons.forEach(btn =>
     btn.addEventListener('click', grabRatingValue))
 
-submitBtn.addEventListener('click', onSubmitClick);
-
 function grabRatingValue(event) {
     let ratingValue = Number(event.target.textContent);
-    console.log(ratingValue);
+    chosenRating.innerText = ratingValue;
 }
 
 
-function onSubmitClick() {
-
-    ratingCard.style.display = 'none';
-    thankYouCard.style.display = 'flex';
-}
+/*Hover effects code*/
